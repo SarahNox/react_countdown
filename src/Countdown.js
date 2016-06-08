ac86174e2aa6d49b52590b09dfdfd4c505fb4a66
SarahNox <apnoeist@googlemail.com>
2016-06-08 15:27:03 +0200

    display time

--- src/Countdown.js
+++ src/Countdown.js
@@ -7,50 +7,49 @@ var React = require('react');
 //   }
 // });
 
-  // var Dial = require('react-dial-master');
+  var Dial = require('react-dial-master');
 
-  // var SetIntervalMixin = {
-  //   componentWillMount: function() {
-  //     this.intervals = [];
-  //   },
-  //   setInterval: function() {
-  //     this.intervals.push(setInterval.apply(null, arguments));
-  //   },
-  //   clearInterval: function() {
-  //     this.intervals;
-  //   },
-  //   componentWillUnmount: function() {
-  //     this.intervals.forEach(clearInterval);
-  //   }
-  // };
+  var SetIntervalMixin = {
+    componentWillMount: function() {
+      this.intervals = [];
+    },
+    setInterval: function() {
+      this.intervals.push(setInterval.apply(null, arguments));
+    },
+    clearInterval: function() {
+      this.intervals;
+    },
+    componentWillUnmount: function() {
+      this.intervals.forEach(clearInterval);
+    }
+  };
 
 module.exports = React.createClass({
-    // mixins: [SetIntervalMixin], 
-    // getInitialState: function() {
-    //   return {seconds: 15,
-    //           toggle: false,
-    //           buttonLabel: "Start" };
-    // },
-    // handleClick: function() {
-    //   if(this.state.toggle){
-    //     this.setState({toggle: false, buttonLabel: "Start"});
-    //     clearInterval(this.intervals);
-    //   } else {
-    //     this.setState({toggle: true, buttonLabel: "Pause"});
-    //     this.setInterval(this.tick, 1000);
-    //   }
-    // },
-    // tick: function() {
-    //   this.setState({seconds: this.state.seconds - 1});
-    // },
-    render: function() {
-      return(<p> Inside the countdown component </p>);
-      //       if (this.state.seconds > 0) {
-      //   return
-      //     <p>{this.state.seconds}</p>;
-      // } else {
-      //   return (
-      //     <div>blah</div>
-      //     );
+    mixins: [SetIntervalMixin], 
+    getInitialState: function() {
+      return {seconds: 15,
+              toggle: false,
+              buttonLabel: "Start" };
+    },
+    handleClick: function() {
+      if(this.state.toggle){
+        this.setState({toggle: false, buttonLabel: "Start"});
+        clearInterval(this.intervals);
+      } else {
+        this.setState({toggle: true, buttonLabel: "Pause"});
+        this.setInterval(this.tick, 1000);
       }
+    },
+    tick: function() {
+      this.setState({seconds: this.state.seconds - 1});
+    },
+    render: function() {
+        if (this.state.seconds > 0) {
+        return(<p>{this.state.seconds}</p>);
+      } else {
+        return (
+          <div>Pomodoro finished</div>
+          );
+      }
+    }
 })
