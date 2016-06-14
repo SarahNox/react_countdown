f6ebd1ba2db11ea7aa9034f72cc72a835aaf2c91
SarahNox <apnoeist@googlemail.com>
2016-06-14 13:36:17 +0200

    working circle countdown

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # react_countdown
+
+# Requirements
+
+   npm install -g react
+   npm install -g react-dom
+   npm install -g rc-progress
+   npm install -g browserify
+
+# Development instructions
+
+    browserify -t [ babelify --presets [ react ] ] src/index.js -o build/app.js
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,53 +1,78 @@
 
 var React = require('react');
+var Circle = require('rc-progress').Circle;
 
-var Dial = require('react-dial-master');
+// var Dial = require('react-dial-master');
 
-  var SetIntervalMixin = {
-    componentWillMount: function() {
-      this.intervals = [];
-    },
-    setInterval: function() {
-      this.intervals.push(setInterval.apply(null, arguments));
-    },
-    clearInterval: function() {
-      this.intervals;
-    },
-    componentWillUnmount: function() {
-      this.intervals.forEach(clearInterval);
-    }
-  };
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  clearInterval: function() {
+    this.intervals;
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
+  }
+};
 
 module.exports = React.createClass({
-    mixins: [SetIntervalMixin], 
-    getInitialState: function() {
-      return {seconds: 15,
-              toggle: false,
-              buttonLabel: "Start" };
-    },
-    handleClick: function() {
-      if(this.state.toggle){
-        this.setState({toggle: false, buttonLabel: "Start"});
-        clearInterval(this.intervals);
-      } else {
-        this.setState({toggle: true, buttonLabel: "Pause"});
-        this.setInterval(this.tick, 1000);
-      }
-    },
-    tick: function() {
-      this.setState({seconds: this.state.seconds - 1});
-    },
-    render: function() {
-        if (this.state.seconds > 0) {
-       return (<p>{this.state.seconds}
-          <Dial value="30" angleOffset={0} angleArc={360} readOnly={true} fgColor="#000000" />
-          <br></br>
-          <button onClick={this.handleClick}>{this.state.buttonLabel}</button>
-          </p>);
-      } else {
-        return (
-          <div>Pomodoro finished</div>
-          );
-      }
+  mixins: [SetIntervalMixin], 
+  getInitialState: function() {
+    return({
+      seconds: this.initialCountdownValue() ,
+      toggle: false,
+      buttonLabel: "Start" 
+    });
+  },
+  handleClick: function() {
+    if(this.state.toggle){
+      this.setState({toggle: false, buttonLabel: "Start"});
+      clearInterval(this.intervals);
+    } else {
+      this.setState({toggle: true, buttonLabel: "Pause"});
+      this.setInterval(this.tick, 1000);
     }
+  },
+  tick: function() {
+    this.setState({seconds: this.state.seconds - 1});
+  },
+  percent: function(){
+    var originalValue = this.initialCountdownValue(); 
+    var currentValue = this.state.seconds;
+    return(currentValue / originalValue * 100);
+  },
+  initialCountdownValue: function(){
+   return(15);
+ },
+ render: function() {
+  var circleContainerStyle = {
+    "width": "250px",
+    "height": "250px"
+  }
+  var layout = {
+    "position": "relative",
+    "top": "-150px",
+    "left": "110px",
+    "text": "center"
+  }
+  if (this.state.seconds > 0) {
+    return (
+      <div style={circleContainerStyle}>
+      <Circle percent={this.percent()} strokeWidth="4" strokeColor="#ff0000" />
+      <div style={layout}>
+      {this.state.seconds}
+      <br></br>
+      <button onClick={this.handleClick}>{this.state.buttonLabel}</button>
+      </div>
+      </div>);
+  } else {
+    return (
+      <div>Pomodoro finished</div>
+      );
+  }
+}
 })
